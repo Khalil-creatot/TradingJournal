59779dcc3216754c5257f307e5c5f0d83a939ba4
Khalil <s2105725@student.hb.se>
2026-09-22 11:37:09 +0200

    lite ändringar i MainWindow

--- TradingJournal.wpf/Views/JournalView.xaml.cs
+++ TradingJournal.wpf/Views/JournalView.xaml.cs
@@ -11,5 +11,10 @@ namespace TradingJournal.wpf.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
